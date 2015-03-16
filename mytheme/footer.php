3ff5160cfceca6481f02b1d77ebc5b9796186150
unknown <ursucalin92@gmail.com>
2015-03-16 11:11:04 +0100

    Nursery Theme

---
+++ mytheme/footer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying the footer.
+ */
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+	<p>footer</p>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
